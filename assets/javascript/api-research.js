1e78fe87ad48c0e328e713e44701fcba23eb626c
Jeff Palin <jeffpalin@gmail.com>
2017-09-01 09:56:06 -0700

    periodic backup

--- assets/javascript/api-research.js
+++ assets/javascript/api-research.js
@@ -6,6 +6,7 @@ var lat = "";
 var lng = "";
 var urlCurrentWeather = "";
 
+// Grab coordinates on click from HTML form. Edit this to match working index.html
 
 $("#submitCoordinates").on("click", function(event) {
   event.preventDefault();
@@ -13,6 +14,8 @@ $("#submitCoordinates").on("click", function(event) {
   lng = $("#lng").val();
   getWeather(lat, lng);
   });
+
+// AJAX Get based on Lat & Lng 
 
 function getWeather(lat, lng) {
   urlCurrentWeather = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lng + "&appid=" + apiKey;
@@ -24,11 +27,12 @@ function getWeather(lat, lng) {
   }).done(function(response) {
     console.log(response);
 
-      //Temperature in fahrenheit
+      // Selected weather information - can be edited as needed
       currentLocation = response.name;
+      //Temperature in fahrenheit
       currentTemp = 9 / 5 * (parseInt(response.main.temp) - 273) + 32;
       currentHumidity = response.main.humidity;
-      windSpeed = response.wind.speed;
+      windSpeed = 0.621371 * (response.wind.speed);
       weatherType = response.weather[0].description;
       weatherIcon = response.weather[0].icon;
 
